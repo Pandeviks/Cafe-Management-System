51cb9abdc809708b4fb206276dc8e6f01def0380
AKASH <AKASH@AKASH>
2023-12-02 21:50:56 +0530

    First Changes

---
+++ Cafe-Management-Syste/src/main/java/com/cafe/CafeManagementSysteApplication.java
@@ -0,0 +1,13 @@
+package com.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSysteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSysteApplication.class, args);
+	}
+
+}
